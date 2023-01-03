527efebfb911f256655209250effd3cfd6559024
Colabear754 <jky2406@gmail.com>
2023-01-03 16:52:20 +0900

    스케줄러 테스트 쿼리 작성

--- src/main/kotlin/com/colabear754/scheduler_example/mappers/SchedulerMapper.kt
+++ src/main/kotlin/com/colabear754/scheduler_example/mappers/SchedulerMapper.kt
@@ -1,4 +1,10 @@
 package com.colabear754.scheduler_example.mappers
 
+import com.colabear754.scheduler_example.dto.DataDTO
+import org.apache.ibatis.annotations.Mapper
+
+@Mapper
 interface SchedulerMapper {
+    fun insertData()
+    fun selectData(): List<DataDTO?>
 }
