15c97ab6549996473f17d581b784d68fb7b46494
Colabear754 <jky2406@gmail.com>
2023-01-03 16:52:47 +0900

    스케줄러 예제

--- src/main/kotlin/com/colabear754/scheduler_example/controllers/SchedulerController.kt
+++ src/main/kotlin/com/colabear754/scheduler_example/controllers/SchedulerController.kt
@@ -1,4 +1,19 @@
 package com.colabear754.scheduler_example.controllers
 
-class SchedulerController {
+import com.colabear754.scheduler_example.mappers.SchedulerMapper
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Component
+@RestController
+class SchedulerController(private val schedulerMapper: SchedulerMapper) {
+
+    @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
+    @GetMapping("/insert")
+    fun scheduledInsert() = schedulerMapper.insertData()
+
+    @GetMapping("/select")
+    fun getData() = schedulerMapper.selectData()
 }
